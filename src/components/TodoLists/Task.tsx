82bbe9656c1f76154ea0e967e2947ffa2d436164
Mihail Topal <topal_1995@bk.ru>
2024-06-24 16:23:40 +0300

    comeback tasks

--- src/components/TodoLists/Task.tsx
+++ src/components/TodoLists/Task.tsx
@@ -17,7 +17,10 @@ const Task = (props: ITaskProps) => {
       <div> id={props.id}</div>
       <div>title={props.title}</div>
       <div> description={props.description}</div>
-
+      <div> todoListId={props.todoListId}</div>
+      <div>order={props.order}</div>
+      <div> status={props.status}</div>
+      <div>priority={props.priority}</div>
       <div>startDate={props.startDate}</div>
       <div>deadline={props.deadline}</div>
       <div> addedDate={props.addedDate.toString()}</div>
