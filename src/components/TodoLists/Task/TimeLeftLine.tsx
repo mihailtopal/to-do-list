f6da5776396853e0ec5e242588a5dc59a764335e
Mihail Topal <topal_1995@bk.ru>
2024-07-04 02:13:13 +0300

    restyle time left line

--- src/components/TodoLists/Task/TimeLeftLine.tsx
+++ src/components/TodoLists/Task/TimeLeftLine.tsx
@@ -23,16 +23,17 @@ const TimeLeftLine = ({ startDate, deadline, addedDate }: ITimeLeftLine) => {
   let percent = (diffDeadAndNow / diffStartAndDead) * 100;
   if (percent < 0) percent = 0;
   if (dayjsStartDate > dayjsNow) percent = 100;
-  const conicColors: ProgressProps["strokeColor"] = {
-    "0%": "#e32636",
-    "30%": "#ffe58f",
-    "80%": "#8db600",
+  const pickColor = (percent: number) => {
+    if (percent < 30) return "#e32636";
+    else if (percent < 80) return "#ffe58f";
+    else return "#8db600";
   };
 
   return (
     <>
       <Progress
-        strokeColor={conicColors}
+        steps={16}
+        strokeColor={pickColor(percent)}
         strokeWidth={3}
         percent={percent}
         showInfo={false}
